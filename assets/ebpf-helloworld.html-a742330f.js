569528711a7544cf50d74bc1620c5aaa38226b16
zgjsxx <119160524@qq.com>
2023-01-29 06:53:11 +0000

    Deploying to gh-pages from @ zgjsxx/cs_article@f7b96aad42c7fd59ba83189b8989a2b377f98438 🚀

--- assets/ebpf-helloworld.html-a742330f.js
+++
@@ -1,8 +0,0 @@
-import{_ as e,V as a,W as s,a1 as n}from"./framework-d934f75f.js";const i={},l=n(`<h1 id="ebpf-helloworld" tabindex="-1"><a class="header-anchor" href="#ebpf-helloworld" aria-hidden="true">#</a> eBPF Helloworld</h1><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">git</span> clone git@github.com:libbpf/libbpf-bootstrap.git
-<span class="token builtin class-name">cd</span> libbpf-bootstrap/
-<span class="token function">git</span> submodule update <span class="token parameter variable">--init</span> <span class="token parameter variable">--recursive</span>
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token builtin class-name">cd</span> examples/c
-<span class="token function">make</span> minimal
-<span class="token function">sudo</span> ./minimal
-<span class="token function">sudo</span> <span class="token function">cat</span> /sys/kernel/debug/tracing/trace_pipe
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3),c=[l];function t(d,o){return a(),s("div",null,c)}const p=e(i,[["render",t],["__file","ebpf-helloworld.html.vue"]]);export{p as default};
---
+++ assets/mermaid-mindmap.esm.min-dd6cdc8b.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-bd6d0e30.js";import"./framework-8842c3e4.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-4dda171c.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
