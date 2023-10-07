dd03aa7c9b3b2446c5cc27f088d292763f0443f1
zgjsxx <119160524@qq.com>
2023-10-07 02:20:39 +0000

    Deploying to gh-pages from @ zgjsxx/cs_article@bb1b2d9423d243f1fd0619214609d528349c9afb 🚀

---
+++ assets/linux-frequently-used-cmd.html-3c38ecc4.js
@@ -0,0 +1,4 @@
+import{_ as e,V as n,W as a,a0 as s}from"./framework-9a29aaa0.js";const t={},i=s(`<h1 id="linux-高频使用的命令" tabindex="-1"><a class="header-anchor" href="#linux-高频使用的命令" aria-hidden="true">#</a> Linux 高频使用的命令</h1><p>查看一个目录文件的大小</p><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">du</span> <span class="token parameter variable">-h</span> --max-depth<span class="token operator">=</span><span class="token number">1</span> ./test
+
+<span class="token number">1</span>.7M    ./test
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3),l=[i];function r(d,c){return n(),a("div",null,l)}const u=e(t,[["render",r],["__file","linux-frequently-used-cmd.html.vue"]]);export{u as default};
