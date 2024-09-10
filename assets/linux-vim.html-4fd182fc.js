2854bcf937b079451a1fb356ec90bad0a082297b
zgjsxx <119160524@qq.com>
2024-09-10 08:12:50 +0000

    Deploying to gh-pages from @ zgjsxx/cs_article@36320951fce1151a0b662808ef81a83425711256 🚀

--- assets/linux-vim.html-4fd182fc.js
+++ assets/linux-vim.html-4fd182fc.js
@@ -6,7 +6,7 @@ import{_ as a,V as e,W as i,a0 as s}from"./framework-9a29aaa0.js";const n={},d=s
 <span class="token number">3</span>. 编辑文件。
 <span class="token number">4</span>. :%<span class="token operator">!</span>xxd <span class="token parameter variable">-r</span> 将文件转换回二进制格式。
 <span class="token number">5</span>. 保存退出即可。
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="vim-多窗口之间进行移动" tabindex="-1"><a class="header-anchor" href="#vim-多窗口之间进行移动" aria-hidden="true">#</a> vim 多窗口之间进行移动</h2><p>在 Vim 中使用多个窗口（也称为“分屏”）可以极大地提升你的工作效率。以下是一些常用的多窗口管理和移动命令：</p><p>打开新窗口</p><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>:split 或 :sp：水平分割窗口
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="vim-多窗口之间进行移动" tabindex="-1"><a class="header-anchor" href="#vim-多窗口之间进行移动" aria-hidden="true">#</a> vim 多窗口之间进行移动</h2><p>在 Vim 中使用多个窗口（也称为&quot;分屏&quot;）可以极大地提升你的工作效率。以下是一些常用的多窗口管理和移动命令：</p><p>打开新窗口</p><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>:split 或 :sp：水平分割窗口
 :vsplit 或 :vsp：垂直分割窗口
 </code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><p>在窗口之间移动</p><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>Ctrl-w h 或 Ctrl-w 左方向键：移动到左边的窗口
 Ctrl-w j 或 Ctrl-w 下方向键：移动到下面的窗口
